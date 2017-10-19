cab2afc36fedee5855354c4674e537a77bdc6493
jjurman <jjurman@constantcontact.com>
2017-10-19 01:47:25 -0400

    inital tram-one eslint config

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true,
+    'jasmine': true
+  },
+  'semi': [
+    'never'
+  ],
+  'indent': [
+    'error',
+    2
+  ],
+  'capitalized-comments': [
+    'error',
+    'never'
+  ],
+  'arrow-parens': [
+    'off'
+  ],
+  'object-shorthand': [
+    'error',
+    'consistent'
+  ]
+}
